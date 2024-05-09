faa9a4bc8735ec035dc3d1df8657a097f0067428
Megha <98228827+meghalakhwan@users.noreply.github.com>
2024-05-09 14:21:12 +0530

    Add files via upload

---
+++ quizwebsite/index.php
@@ -0,0 +1,148 @@
+<?php
+$insert = false;
+if(isset($_POST['name'])){
+    // Set connection variables
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    // echo "Success connecting to the db";
+
+    // Collect post variables
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    
+    $sql = "INSERT INTO `quiz_website`.`quiz_form` (`name`, `age`, `gender`, `email`, `phone`) VALUES ('$name', '$age', '$gender', '$email', '$phone');";
+    // echo $sql;
+
+    // Execute the query
+    if($con->query($sql) == true){
+        // echo "Successfully inserted";
+
+        // Flag for successful insertion
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    // Close the database connection
+    $con->close();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QUIZ WEBSITE</title>
+    <link rel="stylesheet" href="style.css">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+</head>
+<body>
+
+    <div class="container">
+        <h1>Welcome to Quiz form</h3>
+        <p>Enter your details and submit this form to confirm your participation in the Quiz </p>
+        <?php
+        if($insert == true){
+            
+        }
+       
+    ?>
+        <form action="index.php" method="post">
+            <input type="text" name="name" id="name" placeholder="Enter your name">
+            <input type="text" name="age" id="age" placeholder="Enter your Age">
+            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+            <input type="email" name="email" id="email" placeholder="Enter your email">
+            <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+            <button class="btn">Submit</button>             
+        </form>
+    </div>
+
+
+
+    <!-- start Quiz button -->
+    <div class="start_btn"><button>Start Quiz</button></div>
+
+    <!-- Info Box -->
+    <div class="info_box">
+        <div class="info-title"><span>Some Rules of this Quiz</span></div>
+        <div class="info-list">
+            <div class="info">1. You will have only <span>15 seconds</span> per each question.</div>
+            <div class="info">2. Once you select your answer, it can't be undone.</div>
+            <div class="info">3. You can't select any option once time goes off.</div>
+            <div class="info">4. You can't exit from the Quiz while you're playing.</div>
+            <div class="info">5. You'll get points on the basis of your correct answers.</div>
+        </div>
+        <div class="buttons">
+            <button class="quit">Exit Quiz</button>
+            <button class="restart">Continue</button>
+        </div>
+    </div>
+
+    <!-- Quiz Box -->
+    <div class="quiz_box">
+        <header>
+            <div class="title">Awesome Quiz Application</div>
+            <div class="timer">
+                <div class="time_left_txt">Time Left</div>
+                <div class="timer_sec">15</div>
+            </div>
+            <div class="time_line"></div>
+        </header>
+        <section>
+            <div class="que_text">
+                <!-- Here I've inserted question from JavaScript -->
+            </div>
+            <div class="option_list">
+                <!-- Here I've inserted options from JavaScript -->
+            </div>
+        </section>
+
+        <!-- footer of Quiz Box -->
+        <footer>
+            <div class="total_que">
+                <!-- Here I've inserted Question Count Number from JavaScript -->
+            </div>
+            <button class="next_btn">Next Que</button>
+        </footer>
+    </div>
+
+    <!-- Result Box -->
+    <div class="result_box">
+        <div class="icon">
+            <i class="fas fa-crown"></i>
+        </div>
+        <div class="complete_text">You've completed the Quiz!</div>
+        <div class="score_text">
+            <!-- Here I've inserted Score Result from JavaScript -->
+        </div>
+        <div class="buttons">
+            <button class="restart">Replay Quiz</button>
+            <button class="quit">Quit Quiz</button>
+        </div>
+    </div>
+
+    <!-- Inside this JavaScript file I've inserted Questions and Options only -->
+    <script src="questions.js"></script>
+
+    <!-- Inside this JavaScript file I've coded all Quiz Codes -->
+    <script src="script.js"></script>
+
+</body>
+</html>
+</html>
---
+++ quizwebsite/questions.js
@@ -0,0 +1,186 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: " Which tag creates a check box for a form in HTML?",
+    answer: "input type=checkbox",
+    options: [
+      "checkbox",
+      "input type=checkbox",
+      "input=checkbox",
+      "input checkbox"
+    ]
+  },
+    {
+    numb: 5,
+    question:" A webpage displays a picture. What tag was used to display that picture?",
+    answer:"img",
+    options: [
+      "picture",
+      "image",
+      "img",
+      "src"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "Which attribute can be used with BODY tag to set background color green?" ,
+    answer: "bgcolor=”green”",
+    options: [
+      "background=”green”",
+       "bgcolor=”green”",
+        "vlink=”green”",
+       "None of above"
+    ]
+  },
+
+  
+  {
+    numb: 7,
+    question: "What is the use of Forms in HTML?",
+    answer: "To collect user input",
+    options: [
+      "To display contents of email",
+       "To display animation effect",
+      "To collect user input",
+      "None of the Above"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "HTML is considered as __________ language",
+    answer: "Markup Language",
+    options: [
+      "Programming Language",
+       "OOP Language",
+      "High Level Language",
+      "Markup Language"
+    ]
+  },
+
+  {
+    numb: 9,
+    question:  "The software that can read and render HTML documents is",
+    answer:  "Browser",
+    options: [
+      "Server",
+      "Compiler",
+      "Interpreter",
+      "Browser"
+    ]
+  },
+
+
+  {
+    numb: 10,
+    question: "An ordered list is a ________ list and an unordered list is a ________ list.",
+    answer:"Numbered & bulleted" ,
+    options: [
+      "Bulleted & numbered",
+      "Bulleted & tabular",
+      "Tabular & numbered",
+      "Numbered & bulleted"
+    ]
+  },
+
+  {
+    numb: 11,
+    question:"When creating a Web document, what format is used to express an image’s height and width?",
+    answer: "Pixels",
+    options: [
+      "Centimeters",
+      "Pixels",
+      "Dots per inch",
+      "Inches"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "_________ connects web pages.",
+    answer:  "Hyperlink",
+    options: [
+      "Connector",
+       "Link",
+       "Hyperlink",
+       "None of the above"
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "Which of the following is an attribute of <Table> tag? ",
+    answer: "CELLPADDING",
+    options: [
+      "SRC",
+      "LINK",
+      "CELLPADDING",
+      "BOLD"
+       
+
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "HTML was first proposed in year _________.",
+    answer: "1990" ,
+    options: [
+      "1980",
+      "1990",
+      "1995",
+      "2000"
+    ]
+  },
+
+  
+  {
+    numb: 15,
+    question:" Which attribute you’ll use with TD tag to merge two cells horizontally?" ,
+    answer: "colspan=2", 
+    options: [
+      "merge=colspan2",
+     "rowspan=2",
+     "colspan=2",
+    "merge=row2"
+    ]
+  },
+
+  
+ 
+];
